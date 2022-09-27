3c35e1e99158bb8823c82f8d37b7ec62884cd299
Luiz Amorim <luiz.simples@gmail.com>
2022-09-27 09:42:17 -0400

    return te numbers

--- src/getnumber.js
+++ src/getnumber.js
@@ -1,7 +1,12 @@
-export default (letter) => {
-  const letters ={
-    'E': 3, 
-    'T': 8
-  }
-  return letters[letter]
+export default (word) => {
+  const wordToArr = word.split('')
+  
+  const wordToNumber = wordToArr.map((letter) => letters[letter]).join('')
+
+  return wordToNumber
+}
+
+const letters ={
+  'E': 3, 
+  'T': 8
 }
--- tests/src/getnumber.test.js
+++ tests/src/getnumber.test.js
@@ -2,10 +2,14 @@ import getnumber from '../../src/getnumber.js'
 
 describe('getnumber', () => {
     it('Get a number from this letter: T', () => {
-      expect(getnumber('T')).toBe(8)
+      expect(getnumber('T')).toBe('8')
     })
 
     it('Get a number from this letter: E', () => {
-      expect(getnumber('E')).toBe(3)
+      expect(getnumber('E')).toBe('3')
+    })
+
+    it('Get a number from this letter: TE', () => {
+      expect(getnumber('TE')).toBe('83')
     })
   })
